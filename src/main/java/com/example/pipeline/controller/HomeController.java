3fd6cf0b3e8d9db7ec4a2ae18f882e4644e5815e
Long Tran <long.thanh.tran@gmail.com>
2017-08-02 21:53:23 +0700

    apply maven docker plugin

--- src/main/java/com/example/pipeline/controller/HomeController.java
+++ src/main/java/com/example/pipeline/controller/HomeController.java
@@ -1,9 +1,9 @@
 package com.example.pipeline.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
     
     @RequestMapping(value="/")
